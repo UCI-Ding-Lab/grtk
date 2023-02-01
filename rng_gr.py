d63e10e1b92995b0f5487eaeea7efba34488ec20
Yicheng Ding <yiched4@uci.edu>
2023-02-01 13:38:26 -0800

    Delete rng_gr.py

--- rng_gr.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def twenty_megabytes():
-    lines = 1400000
-    with open("big_test_data.gr", "w") as file:
-        file.writelines([f"{int(i)} {random.randint(1,999999)}\n" for i in range(lines)])

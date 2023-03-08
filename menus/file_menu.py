eabd5273a78afce3300687526ef54afb77fee061
Guanchen Wu <guanchen2002@gmail.com>
2023-03-08 03:04:21 -0800

    Recent Menu mostly working

--- menus/file_menu.py
+++ menus/file_menu.py
@@ -3,33 +3,90 @@ from tkinter import filedialog as fd
 import tkinter.messagebox
 import timeit
 from bin.data_plot_new import line_container
+import os
+# from queue import Queue
 
 def do_nothing():
     pass
+
+class RecentFilesManager():
+    def __init__(self, FileMenu):
+        self.q = []
+        self.max_size = 5
+        self.FileMenu = FileMenu
+        self.menu = None
+        self.file_path = r"data/recent_paths.txt"
+        self.file = None
+        # self.read()
+        # self.write()
+        # self.read()
+
+    def add(self, item):
+        self.read()
+        if item in self.q:
+            self.q.remove(item)
+        if len(self.q) >= self.max_size:
+            self.q.pop()
+        self.q.insert(0, item)
+        temp = ""
+        for i in self.q:
+            temp = temp + i + "\n"
+        self.file = open(self.file_path, "w")
+        self.file.write(temp)
+        self.file.close()
+        self.menu.delete(0, "end")
+        for i in self.q:
+            self.menu.add_command(label=i, command=lambda: self.FileMenu._load_gr_file(i))
+
+    def read(self):
+        self.q = []
+        if os.path.exists(self.file_path) == False:
+            self.file = open(self.file_path, "w")
+            self.file.close()
+        self.file = open(self.file_path, "r")
+        lines = self.file.readlines()
+        for i in lines:
+            self.q.append(i.strip())
+        self.file.close()
+
+    def add_recent_menu(self):
+        if self.menu == None:
+            self.menu = tkinter.Menu(self.FileMenu.fm, tearoff=0)
+        self.read()
+        for i in self.q:
+            self.menu.add_command(label=i, command=lambda: self.FileMenu._load_gr_file(i))
+        self.FileMenu.fm.add_cascade(label="Recent Gr File", menu=self.menu)
+
+        # print(self.q)
+
+    # def write(self):
+    #     self.file = open(self.file_path, "w")
+    #     self.file.write("hello world:\n")
+    #     self.file.close()
 
 class FileMenu():
     def __init__(self, GUI, container: line_container):
         self.GUI = GUI
         self.line_container = container
-        self.recent_menu = None
-        self.recent_files = []
+
+        self.fm = None
+        self.RFM = RecentFilesManager(self)
         self._init_file_menu()
+        
 
     def _init_file_menu(self):
-        fm = tkinter.Menu(self.GUI.menu_bar, tearoff=0)
-        fm.add_command(label="Test OpenGL", command=do_nothing)
-        fm.add_command(label="Load Gr File", command=self._load_gr_file)
-        #fm.add_command(label="Recent Gr File", command=do_nothing)
-        self.recent_menu = tkinter.Menu(fm, tearoff=0)
-        self._recent_gr_file(self.recent_menu)
-        fm.add_cascade(label="Recent Gr File", menu=self.recent_menu)
+        self.fm = tkinter.Menu(self.GUI.menu_bar, tearoff=0)
+        self.fm.add_command(label="Test OpenGL", command=do_nothing)
+        self.fm.add_command(label="Load Gr File", command=self._load_gr_file)
+        
+        self.RFM.add_recent_menu()
 
-        fm.add_separator()
-        fm.add_command(label="Load Plug'in", command=do_nothing)
-        fm.add_command(label="Unload Plug'in", command=do_nothing)
-        fm.add_separator()
-        fm.add_command(label="Quit All Windows", command=self.GUI.root.quit)
-        self.GUI.menu_bar.add_cascade(label="File", menu=fm)
+        self.fm.add_separator()
+        self.fm.add_command(label="Load Plug'in", command=do_nothing)
+        self.fm.add_command(label="Unload Plug'in", command=do_nothing)
+        self.fm.add_separator()
+        self.fm.add_command(label="Quit All Windows", command=self.GUI.root.quit)
+        self.GUI.menu_bar.add_cascade(label="File", menu=self.fm)
 
     def _load_gr_file(self, file_path=None):
         try:
@@ -43,28 +100,9 @@ class FileMenu():
                     title='Open a file',
                     initialdir='/',
                     filetypes=filetypes)
-            
-            try:
-                data_file = open(r"data/recent_paths.txt", "r")
-                self.recent_files = []
-                while True:
-                    line = data_file.readline()
-                    if not line:
-                        data_file.close()
-                        break
-                    self.recent_files.append(line)
-            except:
-                pass
-
-            if file_path not in self.recent_files :
-                append_line = file_path + "\n"
-                data_file = open(r"data/recent_paths.txt", "a")
-                data_file.write(append_line)
-                data_file.close()
-                self.recent_files.append(file_path)
-                self._recent_gr_file(self.recent_files, file_path)
 
 
+            self.RFM.add(file_path)
 
             start = timeit.default_timer()
             self.line_container.load_and_plot(path=file_path)
@@ -75,17 +113,3 @@ class FileMenu():
             if self.GUI.debug_mode:
                 self.GUI.log._log("ERROR: "+repr(e))
             raise e
-
-
-    def _recent_gr_file(self, menu, file_path=None):
-        if file_path != None:
-            self.recent_menu.add_command(label=file_path, command=lambda: self._load_gr_file(file_path))
-        else:
-            try:
-                data_file = open(r"data/recent_paths.txt", "r")
-                for i in data_file.readlines():
-                    menu.add_command(label=i, command=lambda: self._load_gr_file(i.strip()))
-                data_file.close()
-            except:
-                pass
-        # menu.add_command(label="test 1", command=do_nothing)

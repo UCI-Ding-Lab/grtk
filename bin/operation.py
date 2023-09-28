9268c3e9de3957e108037a042c8fbf99950d10ba
Yicheng Ding <yiched4@uci.edu>
2023-09-28 10:30:00 -0700

    Revert "Several updates"
    This reverts commit b08b396e2bf3e2483f75b9b07bd85c931ab8712f.

--- bin/operation.py
+++ bin/operation.py
@@ -20,17 +20,64 @@ class operations:
     CURVE = 0
     def __init__(self):
         pass
-
-    def _refresh_listbox(self, gui, listbox):
-        listbox.delete(0, tk.END)
-        for i in gui.container.get_curves_list():
-            temp_str = f"{pathlib.Path(i[0]).name}/{i[1]}/{i[2]}"
-            listbox.insert(tk.END, temp_str)
-        return None
+    # def opt_subtract(self, target_A: load.single_line, target_B: load.single_line):
+    #     if target_A.abs_cords_x.size != target_B.abs_cords_x.size:
+    #         bigger = target_A if target_A.abs_cords_x.size > target_B.abs_cords_x.size else target_B
+    #     else:
+    #         bigger = target_A
+    #     if target_A.abs_cords_y.size >= target_B.abs_cords_y.size:
+    #         cords_y = np.array(target_A.abs_cords_y[:target_B.abs_cords_y.size] - target_B.abs_cords_y)
+    #         cords_x = bigger.abs_cords_x[:target_B.abs_cords_y.size]
+    #     else:
+    #         cords_y = np.array(target_A.abs_cords_y - target_B.abs_cords_y[:target_A.abs_cords_y.size])
+    #         cords_x = bigger.abs_cords_x[:target_A.abs_cords_y.size]
+    #     target_C: load.single_line = load.single_line(
+    #         file=operations.FILE,
+    #         curve=str(operations.CURVE),
+    #         type=operations.TYPE,
+    #         cords=np.array([cords_x, cords_y]),
+    #         file_path=operations.FILE
+    #     )
+    #     return target_C
+    # def opt_addition(self, target_A: load.single_line, target_B: load.single_line):
+    #     if target_A.abs_cords_x.size != target_B.abs_cords_x.size:
+    #         bigger = target_A if target_A.abs_cords_x.size > target_B.abs_cords_x.size else target_B
+    #     else:
+    #         bigger = target_A
+    #     if target_A.abs_cords_y.size >= target_B.abs_cords_y.size:
+    #         cords_y = np.array(target_A.abs_cords_y[:target_B.abs_cords_y.size] + target_B.abs_cords_y)
+    #         cords_x = bigger.abs_cords_x[:target_B.abs_cords_y.size]
+    #     else:
+    #         cords_y = np.array(target_A.abs_cords_y + target_B.abs_cords_y[:target_A.abs_cords_y.size])
+    #         cords_x = bigger.abs_cords_x[:target_A.abs_cords_y.size]
+    #     target_C: load.single_line = load.single_line(
+    #         file=operations.FILE,
+    #         curve=str(operations.CURVE),
+    #         type=operations.TYPE,
+    #         cords=np.array([cords_x, cords_y]),
+    #         file_path=operations.FILE
+    #     )
+    #     return target_C
     
-    def _pop_operation_result_window(self, window, message):
-        messagebox.showinfo(title="Operation Successful!", message=message, parent=window)
-        return None
+    # def opt_averaging(self, target_A: load.single_line, target_B: load.single_line):
+    #     # print('targ A x:::', target_A.abs_cords_x.size)
+    #     # print('targ A y:::', target_A.abs_cords_y.size)
+    #     # print('targ B x:::', target_B.abs_cords_x.size)
+    #     # print('targ B y:::', target_B.abs_cords_y.size)
+    #     if target_A.abs_cords_x.size != target_B.abs_cords_x.size:
+    #         messagebox.showerror("Operation Unsuccessful", "The selected curves have different sizes:\n" + \
+    #             f"{target_A.nick}(x, y): ({target_A.abs_cords_x.size}, {target_A.abs_cords_y.size})\n" + \
+    #             f"{target_B.nick}(x, y): ({target_B.abs_cords_x.size}, {target_B.abs_cords_y.size})")
+    #         return None
+    #     coords_y = (target_A.abs_cords_y + target_B.abs_cords_y) / 2
+    #     target_C: load.single_line = load.single_line(
+    #         file=operations.FILE,
+    #         curve=str(operations.CURVE),
+    #         type=operations.TYPE,
+    #         cords=np.array([target_A.abs_cords_x, coords_y]),
+    #         file_path=operations.FILE
+    #     )
+    #     return target_C
     
     def _get_result_curve(self, file, curve, type, cords_x, cords_y, file_path):
         result_curve: load.single_line = load.single_line(
@@ -50,8 +97,7 @@ class operations:
         minuend_curves = [listbox1.get(idx) for idx in listbox1.curselection()]
         subtrahend_curves = [listbox2.get(idx) for idx in listbox2.curselection()]
         
-        subtrahend_message = ""
-        message = "Below operations are performed:\n\n"
+        # result_curves = []
         
         if minuend_curves == [] or subtrahend_curves == []:
             return None
@@ -65,7 +111,6 @@ class operations:
             else:
                 subtrahend_sum = subtrahend_sum + np.array(gui.container.\
                     container[file_name][type_name][nick_name].abs_cords_y)
-            subtrahend_message = subtrahend_message + f" - {file_name}/{type_name}/{nick_name}"
             
         for i in minuend_curves:
             file_name, type_name, nick_name = i.split('/')
@@ -85,34 +130,51 @@ class operations:
                 str(operations.CURVE), operations.TYPE, cords_x, cords_y, operations.FILE)
             gui.container.load_and_plot_obj(target=result_curve)
             operations.CURVE += 1
-            message = message + f"{file_name}/{type_name}/{nick_name}" + subtrahend_message + \
-                f" = {operations.FILE}/{operations.TYPE}/{str(operations.CURVE)}\n\n"
 
-        self._refresh_listbox(gui, listbox1)
-        self._refresh_listbox(gui, listbox2)
-        self._pop_operation_result_window(window, message)
-        # window.destroy()
+        window.destroy()
         return None
 
         
     
     def opt_subtraction(self, gui):
+        # top = tk.Toplevel(gui.root)
+        # top.geometry("750x250")
+        # top.title("Subtraction")
+        
+        # frame = tk.Frame(top)
+        # frame.pack(pady=10)
 
+        # # Create a scroll bar
+        # scrollbar = tk.Scrollbar(frame)
+        # scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # listbox = tk.Listbox(frame, selectmode=tk.MULTIPLE, yscrollcommand=scrollbar.set)
+        # listbox.pack(side=tk.LEFT)
+
+        # # Configure the scroll bar
+        # scrollbar.config(command=listbox.yview)
+        
+        
+        
+        # button1=tk.Button(top,text='Cancel',width=15,height=1, command=do_nothing)
+        # button1.pack(side='right', anchor='sw', padx=10, pady=10)
+        # button2=tk.Button(top,text='Confirm',width=15,height=1, command=do_nothing)
+        # button2.pack(side='right', anchor='se', expand=True, padx=10, pady=10)
+        # # print("DONE")
+        # result_curve = None
         
         top = tk.Toplevel(gui.root)
-        top.geometry("500x300")
+        top.geometry("500x250")
         top.title("Subtraction")
 
         frame_lists = tk.Frame(top)
         frame_lists.pack(side='top', expand=True, fill=tk.X)
 
-
-        # subtract from
         frame_list1 = tk.Frame(frame_lists)
         frame_list1.pack(side='left', expand=True, fill=tk.BOTH)
 
-        
-        label1 = tk.Label(frame_list1, text="Subtract From:\n(Select from this list to average)")
+        # subtract from
+        label1 = tk.Label(frame_list1, text="Subtract From:")
         label1.pack()
 
         scrollbar1 = tk.Scrollbar(frame_list1)
@@ -124,11 +186,10 @@ class operations:
         # Configure the scroll bar
         scrollbar1.config(command=listbox1.yview)
 
-        # subtract with
         frame_list2 = tk.Frame(frame_lists)
         frame_list2.pack(side='left', expand=True, fill=tk.BOTH)
-        
-        label2 = tk.Label(frame_list2, text="Subtract With:\n")
+        # subtract with
+        label2 = tk.Label(frame_list2, text="Subtract With:")
         label2.pack()
 
         scrollbar2 = tk.Scrollbar(frame_list2)
@@ -142,15 +203,11 @@ class operations:
 
         frame_buttons = tk.Frame(top)
         frame_buttons.pack(side='bottom', expand=True, fill=tk.X)
-        button1 = tk.Button(frame_buttons, text='Close', width=15, height=1, command=top.destroy)
+        button1 = tk.Button(frame_buttons, text='Cancel', width=15, height=1, command=top.destroy)
         button1.pack(side='right', anchor='sw', padx=10, pady=10)
-        button2 = tk.Button(frame_buttons, text='Subtract', width=15, height=1, \
+        button2 = tk.Button(frame_buttons, text='Confirm', width=15, height=1, \
             command=lambda: self._perform_subtraction(top, gui, listbox1, listbox2))
         button2.pack(side='right', anchor='se', expand=True, padx=10, pady=10)
-       
-        button3 = tk.Button(frame_buttons, text='Average', width=15, height=1, \
-            command=lambda: self._perform_averaging(top, gui, listbox1, listbox2))
-        button3.pack(side='left', anchor='e', padx=10, pady=10)
        
         for i in gui.container.get_curves_list():
             temp_str = f"{pathlib.Path(i[0]).name}/{i[1]}/{i[2]}"
@@ -167,12 +224,6 @@ class operations:
         selected_curves = [listbox1.get(idx) for idx in listbox1.curselection()]
         cords_y = None
         cords_x = None
-        
-        message = "Below operations are performed:\n\n"
-        
-        if selected_curves == []:
-            return None
-        
         for i in selected_curves:
             file_name, type_name, nick_name = i.split('/')
             if cords_y is None:
@@ -183,23 +234,11 @@ class operations:
             else:
                 cords_y = cords_y + np.array(gui.container.\
                     container[file_name][type_name][nick_name].abs_cords_y)
-                
-            if message == "Below operations are performed:\n\n":
-                message = message + f"{file_name}/{type_name}/{nick_name}"
-            else:
-                message = message + f" + {file_name}/{type_name}/{nick_name}"
-            
         result_curve = self._get_result_curve(operations.FILE, \
             str(operations.CURVE), operations.TYPE, cords_x, cords_y, operations.FILE)
         gui.container.load_and_plot_obj(target=result_curve)
         operations.CURVE += 1
-        
-        message = message + f" = {operations.FILE}/{operations.TYPE}/{str(operations.CURVE)}"
-        
-        # window.destroy()
-        self._refresh_listbox(gui, listbox1)
-        self._pop_operation_result_window(window, message)
-        
+        window.destroy()
         return None
     
     def opt_addition(self, gui):
@@ -226,11 +265,24 @@ class operations:
         # Configure the scroll bar
         scrollbar1.config(command=listbox1.yview)
 
+        # frame_list2 = tk.Frame(frame_lists)
+        # frame_list2.pack(side='left', expand=True, fill=tk.BOTH)
+        # # subtract with
+        # label2 = tk.Label(frame_list2, text="Subtract With:")
+        # label2.pack()
 
+        # scrollbar2 = tk.Scrollbar(frame_list2)
+        # scrollbar2.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # listbox2 = tk.Listbox(frame_list2, selectmode=tk.MULTIPLE, yscrollcommand=scrollbar2.set, exportselection=False)
+        # listbox2.pack(expand=True, fill=tk.BOTH)
+
+        # # Configure the scroll bar
+        # scrollbar2.config(command=listbox2.yview)
 
         frame_buttons = tk.Frame(top)
         frame_buttons.pack(side='bottom', expand=True, fill=tk.X)
-        button1 = tk.Button(frame_buttons, text='Close', width=15, height=1, command=top.destroy)
+        button1 = tk.Button(frame_buttons, text='Cancel', width=15, height=1, command=top.destroy)
         button1.pack(side='right', anchor='sw', padx=10, pady=10)
         button2 = tk.Button(frame_buttons, text='Confirm', width=15, height=1, \
             command=lambda: self._perform_addition(top, gui, listbox1))
@@ -246,16 +298,10 @@ class operations:
         return None
     
     
-    def _perform_averaging(self, window, gui, listbox1, listbox2=None):
+    def _perform_averaging(self, window, gui, listbox1):
         selected_curves = [listbox1.get(idx) for idx in listbox1.curselection()]
         cords_y = None
         cords_x = None
-        
-        message = "Below operations are performed:\n\n"
-        
-        if selected_curves == []:
-            return None
-        
         for i in selected_curves:
             file_name, type_name, nick_name = i.split('/')
             if cords_y is None:
@@ -266,26 +312,12 @@ class operations:
             else:
                 cords_y = cords_y + np.array(gui.container.\
                     container[file_name][type_name][nick_name].abs_cords_y)
-            if message == "Below operations are performed:\n\n":
-                message = message + f"({file_name}/{type_name}/{nick_name}"
-            else:
-                message = message + f" + {file_name}/{type_name}/{nick_name}"
-                
         cords_y = cords_y / len(selected_curves)
         result_curve = self._get_result_curve(operations.FILE, \
             str(operations.CURVE), operations.TYPE, cords_x, cords_y, operations.FILE)
         gui.container.load_and_plot_obj(target=result_curve)
         operations.CURVE += 1
-        
-        message = message + f")/{len(selected_curves)} = {operations.FILE}/{operations.TYPE}/{str(operations.CURVE)}"
-        
-        self._refresh_listbox(gui, listbox1)
-        if listbox2 is not None:
-            self._refresh_listbox(gui, listbox2)
-        # window.destroy()
-        
-        self._pop_operation_result_window(window, message)
-        
+        window.destroy()
         return None    
 
     def opt_averaging(self, gui):
@@ -312,11 +344,24 @@ class operations:
         # Configure the scroll bar
         scrollbar1.config(command=listbox1.yview)
 
+        # frame_list2 = tk.Frame(frame_lists)
+        # frame_list2.pack(side='left', expand=True, fill=tk.BOTH)
+        # # subtract with
+        # label2 = tk.Label(frame_list2, text="Subtract With:")
+        # label2.pack()
 
+        # scrollbar2 = tk.Scrollbar(frame_list2)
+        # scrollbar2.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # listbox2 = tk.Listbox(frame_list2, selectmode=tk.MULTIPLE, yscrollcommand=scrollbar2.set, exportselection=False)
+        # listbox2.pack(expand=True, fill=tk.BOTH)
+
+        # # Configure the scroll bar
+        # scrollbar2.config(command=listbox2.yview)
 
         frame_buttons = tk.Frame(top)
         frame_buttons.pack(side='bottom', expand=True, fill=tk.X)
-        button1 = tk.Button(frame_buttons, text='Close', width=15, height=1, command=top.destroy)
+        button1 = tk.Button(frame_buttons, text='Cancel', width=15, height=1, command=top.destroy)
         button1.pack(side='right', anchor='sw', padx=10, pady=10)
         button2 = tk.Button(frame_buttons, text='Confirm', width=15, height=1, \
             command=lambda: self._perform_averaging(top, gui, listbox1))
@@ -333,6 +378,30 @@ class operations:
 
         return None
     
+    # def opt_addition(self, selected_curves: list):
+    #     cords_y = np.sum([i.abs_cords_y for i in selected_curves], axis=0)
+    #     cords_x = selected_curves[0].abs_cords_x
+    #     result_curve: load.single_line = load.single_line(
+    #         file=operations.FILE,
+    #         curve=str(operations.CURVE),
+    #         type=operations.TYPE,
+    #         cords=np.array([cords_x, cords_y]),
+    #         file_path=operations.FILE
+    #     )
+    #     return result_curve
+    
+    # def opt_averaging(self, selected_curves: list):
+    #     selected_curves_sz = len(selected_curves)
+    #     cords_y = np.mean([i.abs_cords_y for i in selected_curves], axis=0)# / selected_curves_sz
+    #     cords_x = selected_curves[0].abs_cords_x
+    #     result_curve: load.single_line = load.single_line(
+    #         file=operations.FILE,
+    #         curve=str(operations.CURVE),
+    #         type=operations.TYPE,
+    #         cords=np.array([cords_x, cords_y]),
+    #         file_path=operations.FILE
+    #     )
+    #     return result_curve
     
     
     
@@ -358,7 +427,27 @@ class operations:
         return None#, None
         
     def menu_perform(self, gui: "main.GUI", action: str):
-
+        # selected_curves = self.selection(gui)
+        
+        # # if target_A is not None and target_B is not None:
+        # #     if action == "+":
+        # #         obj = self.opt_addition(target_A, target_B)
+        # #     elif action == "-":
+        # #         obj = self.opt_subtract(target_A, target_B)
+        # #     elif action == "AVG":
+        # #         obj = self.opt_averaging(target_A, target_B)
+        # #     gui.container.load_and_plot_obj(target=obj)
+        # #     operations.CURVE += 1
+        # if selected_curves is not None or action == "-":
+        #     if action == "+":
+        #         obj = self.opt_addition(selected_curves)
+        #     elif action == "-":
+        #         obj = self.opt_subtraction(gui) #, selected_curves
+        #     elif action == "AVG":
+        #         obj = self.opt_averaging(selected_curves)
+        #     gui.container.load_and_plot_obj(target=obj)
+        #     operations.CURVE += 1
+        # return None
         obj = None
         if action == "+":
             self.opt_addition(gui)
@@ -366,7 +455,13 @@ class operations:
             self.opt_subtraction(gui) #, selected_curves
         elif action == "AVG":
             self.opt_averaging(gui)
-
+        # if obj is not None:
+        #     gui.container.load_and_plot_obj(target=obj)
+        #     operations.CURVE += 1
+        # if type(obj) is list:
+        #     for i in obj:
+        #         gui.container.load_and_plot_obj(target=i)
+        #         operations.CURVE += 1
         return None
     
     def cus_perform(self, gui: "main.GUI", cus):

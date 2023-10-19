d6692cb05c9a6661aab8bebe5856a10c2c2cde1e
Yicheng Ding <yiched4@uci.edu>
2023-10-18 23:01:34 -0700

    Gparse Revised

--- gparse.py
+++ gparse.py
@@ -1,6 +1,7 @@
 import numpy as np
+import os
 
-def gparse(H: bool, arg: str, raw: str) -> str:
+def gparse(arg: str, raw: str, f: str, sep: str) -> str:
     """GRTK INPUT PARSING
 
     Args:
@@ -10,50 +11,61 @@ def gparse(H: bool, arg: str, raw: str) -> str:
     Returns:
         str: _description_
     """
-    if H:
-        fm = arg.split(",")
-        allCurves: dict[str,np.ndarray] = {}
-        ## --- ##
-        toList: list[str] = raw.split("\n")
-        rowCount = len(toList)
-        colCount = len(toList[0].split())
-        if len(fm) != colCount:
-            raise ValueError
-        proc = np.empty([rowCount,colCount])
-        for indRow, Row in enumerate(toList):
-            for indCol, Col in enumerate(Row.split()):
-                proc[indRow,indCol] = float(Col)
-        ## --- ##
-        for indRow in range(colCount):
-            allCurves[fm[indRow]] = proc.T[indRow]
-        ## --- ##
+    fm = arg.split(",")
+    allCurves: dict[str,np.ndarray] = {}
+    ## --- ##
+    toList: list[str] = raw.split("\n")
+    rowCount = len(toList)
+    colCount = len(toList[0].split())
+    if len(fm) != colCount:
+        raise ValueError
+    proc = np.empty([rowCount,colCount])
+    for indRow, Row in enumerate(toList):
+        for indCol, Col in enumerate(Row.split()):
+            proc[indRow,indCol] = float(Col)
+    ## --- ##
+    for indRow in range(colCount):
+        allCurves[fm[indRow]] = proc.T[indRow]
+    ## --- ##
+    build: dict[str,np.ndarray] = {}
+    with open(f+"_parsed.gr","w") as fNew:
+        empty = True
         for key, val in allCurves.items():
-            print(key, val)
-    else:
-        fm: list[str] = arg.split(",")
-        allCurves: dict[str,np.ndarray] = {}
-        sepBuild = fm[0][0]*int(fm[0].split("x")[1])+"\n"
-        curvName = fm[1:]
-        setCount = len(curvName)
-        rawProc = raw.split(sepBuild)
-        for indSet in range(setCount):
-            rawProc[indSet] = rawProc[indSet][:-1].split("\n")
-            singleSet = np.ndarray([len(rawProc[indSet]),2])
-            if len(rawProc[indSet][-1].split()) == 2:
-                for indRow, Row in enumerate(rawProc[indSet]):
-                    singleSet[indRow] = np.array(Row.split()).astype(float)
-            else:
-                for indRow, Row in enumerate(rawProc[indSet]):
-                    singleSet[indRow] = np.array([indRow,Row]).astype(float)
-            allCurves[curvName[indSet]] = singleSet
-        ## --- ##
-        for key, val in allCurves.items():
-            print(key, val)
+            if key != "time":
+                build[key] = np.vstack((allCurves["time"],val)).T
+                rows = ["{} {}".format(a, b) for a, b in build[key]]
+                if "_" not in key:
+                    curveType = "data"
+                    curveName = key
+                else:
+                    curveType = key.split("_")[1]
+                    curveName = key.split("_")[0]
+                if empty:
+                    text = "\n".join([curveType,curveName,"\n".join(rows)]) + "\n"
+                    fNew.write(text)
+                    empty = False
+                else:
+                    text = "\n".join([sep,curveType,curveName,"\n".join(rows)]) + "\n"
+                    fNew.write(text)
+    ## --- ##
+    print("DONE:", f+"_parsed.gr")
         
 
 
 if __name__ == "__main__":
-    # with open("./UCSD_Data/001") as f:
-    #     gparse(True, "time,c01,c02,c03,c04,c05,c06,c07,c08,c09,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c31,c33,c34,c35",f.read())
-    with open("./UCSD_Data/Testing/demo") as f:
-        gparse(False, "-x10,c01,c02,c03", f.read())
+    directory = './UCSD_Data'
+    for filename in os.listdir(directory):
+        if filename.endswith(".DS_Store"):
+            continue
+        with open(os.path.join(directory, filename)) as f:
+            gparse("time,c01,c02,c03,c04,c05,c06,c07,c08,c09,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c31,c33,c34,c35",f.read(),f.name,"-"*10)
+    
+    # buf = ""
+    # for filename in os.listdir(directory):
+    #     if filename.endswith(".DS_Store"):
+    #         continue
+    #     with open(os.path.join(directory, filename)) as f:
+    #         buf += f.read()+"\n"
+    # with open(os.path.join(directory, "all.gr"),"w") as f:
+    #     f.write(buf)
+        

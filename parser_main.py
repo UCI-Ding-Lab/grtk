93faab137a683177b855a7fc2f71fe181c8622b3
Guanchen Wu <guanchen2002@gmail.com>
2023-10-26 05:26:42 -0700

    Update parser_main.py
    - Added scrollbars
    - Added checkbuttons (bug)

--- parser_main.py
+++ parser_main.py
@@ -45,11 +45,29 @@ from Gparse.gparse import gparse
 def do_nothing():
     return None
 
+class CheckButtonManager():
+    def __init__(self, frame):
+        self.check_buttons = []
+        self.frame = frame
+        self.counter = 1
+        return None
+    
+    def add(self, name, checked=0):
+        self.check_buttons.append(\
+            tk.Checkbutton(self.frame, text=name, command=lambda: None))
+        self.check_buttons[self.counter-1].grid(row=self.counter, column=0, sticky="nw")
+        if checked == 1:
+            self.check_buttons[self.counter-1].select()
+        self.counter += 1
+        # self.radio_buttons.append(temp)
+        # temp = None
+        return None
+
 class ParserGUI():
     def __init__(self, root):
         self.root = root
         self.root.title('Parser')
-        self.root.geometry("800x500")
+        self.root.geometry("800x600")
         
         self.menu_bar = tk.Menu(root)
         self.file_menu = None
@@ -62,8 +80,8 @@ class ParserGUI():
 
         # frames layout configs
         self.root.grid_rowconfigure(0, weight=1)
-        self.root.grid_rowconfigure(1, weight=1)
-        self.root.grid_rowconfigure(2, weight=1)
+        self.root.grid_rowconfigure(1, weight=5)
+        self.root.grid_rowconfigure(2, weight=5)
         self.root.grid_columnconfigure(0, weight=1)
         
         self.parser = gparse()
@@ -72,59 +90,154 @@ class ParserGUI():
         
     def _init_top_frame(self):
         self.top_frame = tk.LabelFrame(self.root, text="Data Information", labelanchor="nw")
-        self.top_frame.grid(row=0, column=0, sticky='nsew')
+        self.top_frame.grid(row=0, column=0, sticky='nsew', pady=(0, 2))
+        self.top_frame.grid_propagate(False)
         #Data Information:\n highlightbackground="black", highlightthickness=1
+        
+        # # Set up the canvas
+        # self.top_canvas = tk.Canvas(self.top_label_frame)
+        # self.top_canvas.grid(row=0, column=0, sticky="nsew")
+        
+        # # Set the frame inside the canvas
+        # self.top_frame = tk.Frame(self.top_canvas)
+        # self.top_frame.grid(row=0, column=0, sticky="nsew")
+        
+        
         top_placeholder = "Rows:\nColumns:\n" 
         #bg="white", 
-        self.top_label = tk.Label(self.top_frame, text=top_placeholder, anchor='nw', justify='left')
+        self.top_label = tk.Label(self.top_frame, text=top_placeholder, anchor='nw', \
+            justify='left')
         self.top_label.pack(fill=tk.BOTH, expand=True)
     
-    def _init_middle_frame(self):
-        self.middle_frame = tk.LabelFrame(self.root, text="Configuration Information", labelanchor="nw")
-        self.middle_frame.grid(row=1, column=0, sticky='nsew')
+    # def _init_middle_frame(self):
+    #     self.middle_label_frame = tk.LabelFrame(self.root, text="Configuration Information", labelanchor="nw")
+    #     self.middle_label_frame.grid(row=1, column=0, sticky='nsew')
+    #     self.middle_scrollbar = tk.Scrollbar(self.root, orient="vertical")
+    #     self.middle_scrollbar.grid(row=1, column=1, sticky="ns")
         
+    #     self.middle_canvas = tk.Canvas(self.middle_label_frame, \
+    #         yscrollcommand=self.middle_scrollbar.set)
+    #     self.middle_canvas.pack(fill=tk.BOTH, expand=True)
+        
+    #     middle_placeholder = "Assigned Curves:\n"
+    #     self.middle_label = tk.Label(self.middle_canvas, text=middle_placeholder, anchor='nw', justify='left')
+    #     self.middle_label.grid(row=0, column=0, sticky="nw")
+        
+
+        
+    #     self.middle_scrollbar.config(command=self.middle_canvas.yview)
+    
+    def _init_middle_frame(self):
+        self.middle_label_frame = tk.LabelFrame(self.root, text="Configuration Information", \
+            labelanchor="nw")
+        self.middle_label_frame.grid(row=1, column=0, sticky='nsew', pady=2)
+        # self.middle_label_frame.grid_columnconfigure(0, weight=1)  # make the frame expandable
+        self.middle_label_frame.grid_propagate(False)
+        
+        # Set up the scrollbar
+        self.middle_scrollbar = tk.Scrollbar(self.root, orient="vertical")
+        self.middle_scrollbar.grid(row=1, column=1, sticky="ns")
+        
+        # Set up the canvas
+        self.middle_canvas = tk.Canvas(self.middle_label_frame, \
+            yscrollcommand=self.middle_scrollbar.set, \
+            width=800, \
+            height=200)
+        self.middle_canvas.grid(row=0, column=0, sticky="nsew")
+        # self.middle_canvas.grid_propagate(False)
+        
+        # Set the frame inside the canvas
+        self.middle_frame = tk.Frame(self.middle_canvas)
+        self.middle_frame.grid(row=0, column=0, sticky="nsew")
+        
+        # Add the frame to the canvas
+        self.middle_canvas.create_window((0, 0), window=self.middle_frame, anchor="nw")
+        
+        # Configure the scroll region of the canvas whenever it changes
+        self.middle_frame.bind("<Configure>", lambda e: self.middle_canvas.configure(scrollregion=self.middle_canvas.bbox("all")))
+        
+        # Connect the scrollbar to the canvas
+        self.middle_scrollbar.config(command=self.middle_canvas.yview)
+        
+        # Sample label inside the frame
         middle_placeholder = "Assigned Curves:\n"
         self.middle_label = tk.Label(self.middle_frame, text=middle_placeholder, anchor='nw', justify='left')
-        self.middle_label.pack(fill=tk.BOTH, expand=True)
+        self.middle_label.grid(row=0, column=0, sticky="nw")
+
+        
     
     def _init_bottom_frame(self):
         #bg='gray80', 
-        self.bottom_frame = tk.LabelFrame(self.root, text="Configurations", labelanchor="nw")
-        self.bottom_frame.grid(row=2, column=0, sticky='nsew')
+        self.bottom_label_frame = tk.LabelFrame(self.root, text="Configurations", labelanchor="nw")
+        self.bottom_label_frame.grid(row=2, column=0, sticky='nsew', pady=(2, 0))
+        self.bottom_label_frame.grid_propagate(False)
         # highlightbackground="black", highlightthickness=1, 
         # self.config_label = tk.Label(self.bottom_frame, text="Configurations")
         # self.config_label.grid(row=0, column=0, sticky='nw')
 
+
+        # Set up the scrollbar
+        self.bottom_scrollbar = tk.Scrollbar(self.root, orient="vertical")
+        self.bottom_scrollbar.grid(row=2, column=1, sticky="ns")
+        
+        # Set up the canvas
+        self.bottom_canvas = tk.Canvas(self.bottom_label_frame, \
+            yscrollcommand=self.bottom_scrollbar.set, \
+            width=750, \
+            height=200)
+        self.bottom_canvas.grid(row=0, column=0, sticky="nsew")
+        # self.middle_canvas.grid_propagate(False)
+        
+        # Set the frame inside the canvas
+        self.bottom_frame = tk.Frame(self.bottom_canvas)
+        self.bottom_frame.grid(row=0, column=0, sticky="nsew")
+        
+        # Add the frame to the canvas
+        self.bottom_canvas.create_window((0, 0), window=self.bottom_frame, anchor="nw")
+        
+        # Configure the scroll region of the canvas whenever it changes
+        self.bottom_frame.bind("<Configure>", \
+            lambda e: self.bottom_canvas.configure(scrollregion=self.bottom_canvas.bbox("all")))
+        
+        # Connect the scrollbar to the canvas
+        self.bottom_scrollbar.config(command=self.bottom_canvas.yview)
+
+
+        self.curve_names_label = tk.Label(self.bottom_frame, text="Assign Names: ",  anchor='nw', justify='left')
+        self.curve_names_label.grid(row=0, column=0, sticky="nw")
+  
+        self.curve_names_textbox = tk.Text(self.bottom_frame, height=1, width=70)
+        self.curve_names_textbox.grid(row=0, column=1, sticky="nw")
+
+
         self.button1 = tk.Button(self.bottom_frame, text="Load", command=lambda: self._file_menu_load())
-        self.button1.grid(row=2, column=2, sticky='se', padx=5, pady=5)  # using grid instead of pack and modified sticky attribute
+        self.button1.grid(row=0, column=2, sticky='ne', padx=5, pady=0)  # using grid instead of pack and modified sticky attribute
         #column=0, 
         self.button3 = tk.Button(self.bottom_frame, text="Apply", command=lambda: self._file_menu_apply())
-        self.button3.grid(row=2, column=3, sticky='se', padx=5, pady=5)
+        self.button3.grid(row=0, column=3, sticky='ne', padx=5, pady=0)
         # 
         self.button2 = tk.Button(self.bottom_frame, text="Export", command=lambda: self._file_menu_export())
-        self.button2.grid(row=2, column=4, sticky='se', padx=5, pady=5)  # using grid instead of pack and modified sticky attribute
+        self.button2.grid(row=0, column=4, sticky='ne', padx=5, pady=0)  # using grid instead of pack and modified sticky attribute
         # 
         #bg="white",  bg='gray80',
         
 
         
-        self.curve_names_label = tk.Label(self.bottom_frame, text="Assigned Amount:\nAssign Names: ",  anchor='nw', justify='left')
-        self.curve_names_label.grid(row=1, column=0, sticky="nw")
+        # # self.curve_amount_label = tk.Label(self.bottom_frame, text="Assigned Amount: ",  anchor='nw', justify='left')
+        # # self.curve_amount_label.grid(row=0, column=0, sticky="nw")
         
 
-        
-        self.curve_names_textbox = tk.Text(self.bottom_frame, height=10, width=70)
-        self.curve_names_textbox.grid(row=1, column=1, sticky="nw")
- 
+        # # self.curve_names_textbox_sb = tk.Scrollbar(self.bottom_frame, orient='horizontal')
+        # # self.curve_names_textbox_sb.grid(row=1, column=1, sticky="nwe")
         
         
         
-        # self.bottom_frame.grid_rowconfigure(0, weight=1)
-        self.bottom_frame.grid_columnconfigure(0, weight=1)  # setting the weight of the leftmost column
-        self.bottom_frame.grid_columnconfigure(1, weight=1)
-        self.bottom_frame.grid_rowconfigure(0, weight=1)  # setting the weight of the topmost row to push buttons to the bottom
-        self.bottom_frame.grid_rowconfigure(1, weight=1)
-        self.bottom_frame.grid_rowconfigure(2, weight=1)
+        # # self.bottom_frame.grid_rowconfigure(0, weight=1)
+        # self.bottom_frame.grid_columnconfigure(0, weight=1)  # setting the weight of the leftmost column
+        # self.bottom_frame.grid_columnconfigure(1, weight=1)
+        # self.bottom_frame.grid_rowconfigure(0, weight=1)  # setting the weight of the topmost row to push buttons to the bottom
+        # self.bottom_frame.grid_rowconfigure(1, weight=1)
+        # self.bottom_frame.grid_rowconfigure(2, weight=1)
         
         
         
@@ -171,6 +284,9 @@ class ParserGUI():
         # self.parser.proc_group(file_paths)
         self.row_count = 0
         self.col_count = 0
+        self.cbm = CheckButtonManager(self.bottom_frame)
+        
+        
         for f in self.file_paths:
             temp = self.parser.get_shape(f)
             self.row_count += temp[0]
@@ -178,8 +294,10 @@ class ParserGUI():
         self.top_label.config(text=f"Rows: {self.row_count}\nColumns: {self.col_count}\n")
         
         default_assigned_names = "time"
+        self.cbm.add("time", 1)
         for i in range(self.col_count-1):
             default_assigned_names += f"/{i}"
+            self.cbm.add(i)
         self.curve_names_textbox.insert(tk.INSERT, default_assigned_names)
         
         return None
@@ -191,7 +309,7 @@ class ParserGUI():
         self.assigned_names = user_input.split("/")
         temp_text = f"Assigned Amount: {len(self.assigned_names)}\nAssigned Names: "
         for i in self.assigned_names:
-            temp_text += f" -{i} "
+            temp_text += f" -{i} \n"
         self.middle_label.config(text=temp_text)
         return None
     
